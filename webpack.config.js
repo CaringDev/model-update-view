5991254d393bb047dbbb13b763efef146bc39689
Raphael Schweizer <code@caring.dev>
2020-12-28 23:04:45 +0100

    update to Fable 3

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,13 @@ function resolve(filePath) {
   return path.join(__dirname, filePath)
 }
 
-var babelOptions = {
-  presets: [["@babel/preset-env", { "modules": false }]]
-};
-
 var isProduction = process.argv.indexOf("-p") >= 0;
 console.log("Bundling for " + (isProduction ? "production" : "development") + "...");
 
 module.exports = {
   mode: isProduction ? "production" : "development",
   devtool: "source-map",
-  entry: resolve('./src/muv.fsproj'),
+  entry: resolve('./src/App.fs.js'),
   output: {
     filename: 'bundle.js',
     path: resolve('./public'),
@@ -25,23 +21,6 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.fs(x|proj)?$/,
-        use: {
-          loader: "fable-loader",
-          options: {
-            babel: babelOptions,
-          }
-        }
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: babelOptions
-        },
-      },
       {
         test: /\.sass$/,
         use: [
