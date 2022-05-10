24c27d6ede8b23df2a896de9483234c8f7ddd2f8
Raphael Schweizer <code@caring.dev>
2022-05-10 07:57:56 +0200

    update dependencies / tooling

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,7 @@ function resolve(filePath) {
   return path.join(__dirname, filePath)
 }
 
-var isProduction = process.argv.indexOf("-p") >= 0;
-console.log("Bundling for " + (isProduction ? "production" : "development") + "...");
-
 module.exports = {
-  mode: isProduction ? "production" : "development",
   devtool: "source-map",
   entry: resolve('./src/App.fs.js'),
   output: {
